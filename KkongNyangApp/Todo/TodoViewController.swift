b8a92f5aeec127ede71dab72968cb370517252f3
Kim Doy <heydoyhey@gmail.com>
2022-05-27 15:33:38 +0900

    todo 화면 기존 내용 전부제거 - segemented control 이용해서 새로 짤 예정

--- KkongNyangApp/Todo/TodoViewController.swift
+++ KkongNyangApp/Todo/TodoViewController.swift
@@ -10,118 +10,15 @@ import UIKit
 class TodoViewController: UIViewController {
 
     // MARK: - Properties
-    @IBOutlet weak var collectionView: UICollectionView!
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupCollectionView()
 
     }
-    private func setupCollectionView() {
-        
-        // Delegate 연결
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        
-        // 투두 셀 등록
-        collectionView.register(
-            UINib(nibName: "TodoCollectionViewCell",
-                  bundle: nil),
-            forCellWithReuseIdentifier: TodoCollectionViewCell.identifier)
-        
-        // 피니시드 리스트 셀 등록
-        collectionView.register(
-            UINib(nibName: "FinishedListCollectionViewCell",
-                  bundle: nil),
-            forCellWithReuseIdentifier: FinishedListCollectionViewCell.identifier)
-        
-        
-    }
+    
+    // MARK: - Actions
+    
+    
+    // MARK: - Helpers
 }
-
-    // MARK: - Extension Collection View
-extension TodoViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-    // 섹션의 갯수
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
-    }
-    
-    
-    // 셀의 갯수
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        switch section {
-        case 0:
-            return 5
-        default:
-            return 1
-        }
-    }
-
-    
-    // 셀 생성
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let section = indexPath.section
-        switch section {
-        case 0:
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TodoCollectionViewCell.identifier, for: indexPath) as? TodoCollectionViewCell else {
-                fatalError("Cell Type Casting Failed..")
-            }
-            return cell
-                
-        default:
-                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FinishedListCollectionViewCell.identifier, for: indexPath) as? FinishedListCollectionViewCell else {
-                    fatalError("Cell Type Casting Failed..")
-                }
-                return cell
-        }
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        guard let collectionViewCell = cell as? FinishedListCollectionViewCell else {
-            return
-        }
-        collectionViewCell.setCollectionViewDataSourceDelegate(dataSourceDelegate: self, forRow: indexPath.row)
-    }
-
-
-// 셀 크기
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        
-        let section = indexPath.section
-        switch section {
-        case 0:
-            let inset = CGFloat(20)
-            return CGSize(width: (collectionView.frame.width - (inset*2)), height: CGFloat(72))
-        default:
-            return CGSize(width: CGFloat(74), height: CGFloat(74))
-            
-        }
-    }
-    
-    // 아이템 열간격
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        
-        switch section {
-        case 0:
-            return CGFloat(0)
-        default:
-            return CGFloat(14)
-        }
-    }
-    
-    // 아이템 행간
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        switch section {
-        case 0:
-            return CGFloat(8)
-        default:
-            return CGFloat(14)
-        }
-    }
-}
-
-
-
