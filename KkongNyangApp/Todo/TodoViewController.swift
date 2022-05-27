156602bda4a63c0939b6415d318586e8bee97909
Kim Doy <heydoyhey@gmail.com>
2022-05-27 23:24:43 +0900

    투두뷰-세그먼티드컨트롤, 뷰 연결

--- KkongNyangApp/Todo/TodoViewController.swift
+++ KkongNyangApp/Todo/TodoViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 class TodoViewController: UIViewController {
 
     // MARK: - Properties
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var secondView: UIView!
+    @IBOutlet weak var thirdView: UIView!
+    
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -19,6 +23,21 @@ class TodoViewController: UIViewController {
     
     // MARK: - Actions
     
+    @IBAction func switchViews(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            firstView.alpha = 1
+            secondView.alpha = 0
+            thirdView.alpha = 0
+        } else if sender.selectedSegmentIndex == 1 {
+            firstView.alpha = 0
+            secondView.alpha = 1
+            thirdView.alpha = 0
+        } else {
+            firstView.alpha = 0
+            secondView.alpha = 0
+            thirdView.alpha = 1
+        }
+    }
     
     // MARK: - Helpers
 }
