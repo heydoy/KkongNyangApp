e0aa4e1f1b2d2ce566b5461094dd0ed7b4a91117
Kim Doy <heydoyhey@gmail.com>
2022-06-02 23:42:37 +0900

    투두에서 컬렉션 뷰 셀 생성, 코드 생성 연결

---
+++ KkongNyangApp/Todo/Cell/TodoListCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TodoListCollectionViewCell.swift
+//  KkongNyangApp
+//
+//  Created by Doy Kim on 2022/06/02.
+//
+
+import UIKit
+
+class TodoListCollectionViewCell: UICollectionViewCell {
+    
+}
