15b7571cd5f4c296435eb1e6310d580003de18e1
Kim Doy <heydoyhey@gmail.com>
2022-06-02 23:49:50 +0900

    셀 요소를 아울렛으로 연결

--- KkongNyangApp/Todo/Cell/TodoListCollectionViewCell.swift
+++ KkongNyangApp/Todo/Cell/TodoListCollectionViewCell.swift
@@ -8,5 +8,15 @@
 import UIKit
 
 class TodoListCollectionViewCell: UICollectionViewCell {
+    // MARK: - Properties
+    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var todoTitleLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var checkButton: UIButton!
     
+    
+    // MARK: - Actions
+    
+    
+    // MARK: - Helpers
 }
