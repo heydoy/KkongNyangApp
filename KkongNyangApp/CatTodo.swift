8730ca9c540ce54c3fbcf8a8aa74d5cb0f6f171f
Kim Doy <heydoyhey@gmail.com>
2022-06-05 12:01:11 +0900

    catTodo 업데이트, 셀 업데이트

--- KkongNyangApp/CatTodo.swift
+++ KkongNyangApp/CatTodo.swift
@@ -6,14 +6,23 @@
 //
 
 import Foundation
+import UIKit
 
 
 struct CatTodo {
-    var cat: String
-    var todoTitle: String
+    var catID: Int
+    var title: String
+    var time: TimeZone
+    var image: String //아이콘이름
+    var isFinished: Bool
+    var date: String //date 중에 고르기
 }
 
 let TodoTitleList: [String] = [
     "식사주기", "간식주기", "화장실청소", "놀아주기", "병원가기",
     "약 먹이기", "고양이용품 구매", "발톱깎이기 & 귀청소", "기타"
+]
+
+let date: [String] = [
+    "월", "화", "수", "목", "금", "토", "일"
 ]
--- KkongNyangApp/Todo/Cell/TodoListCollectionViewCell.swift
+++ KkongNyangApp/Todo/Cell/TodoListCollectionViewCell.swift
@@ -9,18 +9,29 @@ import UIKit
 
 class TodoListCollectionViewCell: UICollectionViewCell {
     // MARK: - Properties
+    var catId
+    
     @IBOutlet weak var iconImage: UIImageView!
     @IBOutlet weak var todoTitleLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var checkButton: UIButton!
     
     
+    
     // MARK: - Actions
     
     func configure(_ todo: CatTodo) {
         // 데이터를 불러와서 셀을 구성 (FC 프로젝트채팅리스트 2 영상 참조)
-//        todoTitleLabel.text = CatTodo.todoTitle
-//        iconImage.image = UIImage(named: CatTodo.todoTitle)
+        catId = todo.catID
+        iconImage.image = UIImage(named: todo.image)
+        todoTitleLabel.text = todo.title
+        timeLabel.text = "\(todo.time)"
+        
+        if todo.isFinished == true {
+            checkButton.isSelected = true
+        } else {
+            checkButton.isSelected = false
+        }
     }
     
     // MARK: - Helpers
