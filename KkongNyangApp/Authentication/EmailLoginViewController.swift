a3abee4a721c22992b4cb9289d42f577426f5b7a
Kim Doy <heydoyhey@gmail.com>
2022-06-08 17:14:24 +0900

    로그인화면의 가입하기 버튼 글자 일부분 수정 및 uiviewcontroller extension

--- KkongNyangApp/Authentication/EmailLoginViewController.swift
+++ KkongNyangApp/Authentication/EmailLoginViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class EmailLoginViewController: UIViewController {
 
+    
+    // MARK: - Properties
+    var email = String()
+    var password = String()
+    var userInfo: UserInfo?
+    
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //setupAttribute()
+      
     }
+    
+    
+    // MARK: - Actions
 
     @IBAction func didRegisterButtonTapped(_ sender: Any) {
        
@@ -25,7 +38,29 @@ class EmailLoginViewController: UIViewController {
         // 3. 화면전환 메소드를 이용해서 화면을 전환
 //        self.present(registerViewController, animated: true, completion: nil)
         self.navigationController?.pushViewController(registerViewController, animated: true)
-
     }
     
+    
+    // MARK: - Helpers
+    func setupAttribute(){
+        // 가입하기 버튼 색상 변경
+        
+        let text1 = "계정이 없으신가요?"
+        let text2 = "가입하기"
+        
+        let font1 = UIFont.systemFont(ofSize: 13)
+        let font2 = UIFont.boldSystemFont(ofSize: 13)
+        
+        let color1 = UIColor.gray300!
+        let color2 = UIColor.gray700!
+        
+        
+        let attributes = generateButtonAttribute(
+            self.registerButton,
+            texts: text1, text2,
+            fonts: font1, font2,
+            colors: color1, color2
+        )
+        self.registerButton.setAttributedTitle(attributes, for: .normal)
+    }
 }
