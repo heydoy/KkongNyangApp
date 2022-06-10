800bfc17e7896812421be7767d11a27b9c9fd788
Kim Doy <heydoyhey@gmail.com>
2022-06-10 23:13:46 +0900

    기호추가 뷰에 사진선택 넣기

--- KkongNyangApp/CatPalate.swift
+++ KkongNyangApp/CatPalate.swift
@@ -26,20 +26,20 @@ struct CatPalate {
     
 }
 
-enum CatItemKind {
-    case food
-    case snack
-    case toy
-    case sand
-    case etc
-}
+
 
 extension CatPalate {
     static let list: [CatPalate] = [
-        CatPalate(catID: 1, itemKind: "food", itemImage: "sample-palate-1", itemName: "로얄캐닌 센서리필 젤리파우치", preferenceLevel: 0.66, memo: "잘 먹는편", history: "동생 님이 4월 7일 기록"),
-        CatPalate(catID: 2, itemKind: "snack", itemImage: "sample-palate-2", itemName: "챠오츄르 가다랑어맛", preferenceLevel: 1.0, memo: "안 남기고 전부 먹으면서 냥냥댐",  history: "엄마 님이 1월 16일 기록"),
-        CatPalate(catID: 1, itemKind: "toy", itemImage: "sample-palate-3", itemName: "다이소 쥐 장난감", preferenceLevel: 0.3, memo: "3분 놀다가 관심도 안 줌",  history: "동생 님이 어제 기록"),
-        CatPalate(catID: 1, itemKind: "sand", itemImage: "sample-palate-4", itemName: "더스트프리 벤토나이트 파인",  preferenceLevel: 0.0, memo: "잘 안 묻고 싫어함", history: "내가 방금 기록"),
+        CatPalate(catID: 1, itemKind: CatItemKind[0], itemImage: "sample-palate-1", itemName: "로얄캐닌 센서리필 젤리파우치", preferenceLevel: 0.66, memo: "잘 먹는편", history: "동생 님이 4월 7일 기록"),
+        CatPalate(catID: 2, itemKind: CatItemKind[1], itemImage: "sample-palate-2", itemName: "챠오츄르 가다랑어맛", preferenceLevel: 1.0, memo: "안 남기고 전부 먹으면서 냥냥댐",  history: "엄마 님이 1월 16일 기록"),
+        CatPalate(catID: 1, itemKind: CatItemKind[2], itemImage: "sample-palate-3", itemName: "다이소 쥐 장난감", preferenceLevel: 0.3, memo: "3분 놀다가 관심도 안 줌",  history: "동생 님이 어제 기록"),
+        CatPalate(catID: 1, itemKind: CatItemKind[3], itemImage: "sample-palate-4", itemName: "더스트프리 벤토나이트 파인",  preferenceLevel: 0.0, memo: "잘 안 묻고 싫어함", history: "내가 방금 기록"),
     
+    ]
+    
+    static let CatItemKind: [String] = [
+        "음식", "간식",
+        "장난감", "모래",
+        "기타"
     ]
 }
--- KkongNyangApp/Palate/AddPalateViewController.swift
+++ KkongNyangApp/Palate/AddPalateViewController.swift
@@ -6,17 +6,95 @@
 //
 
 import UIKit
+import PickerButton
 
 class AddPalateViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    @IBOutlet weak var selectKindButton: PickerButton!
+    let pickerValues: [String] = CatPalate.CatItemKind
+    
+    let imagePickerViewController = UIImagePickerController()
+    
+    @IBOutlet weak var productImage: UIImageView!
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        selectKindButton.dataSource = self
+        selectKindButton.delegate = self
+        
+        imagePickerViewController.delegate = self
 
     }
     
+    // MARK: - Actions
     @IBAction func didDismissButtonTapped(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func buttonGoImagePicker(_ sender: Any) {
+        let alert = UIAlertController(title: "제품사진", message: "제품 사진을 찍거나 선택해주세요.", preferredStyle: .actionSheet)
+        
+        let library = UIAlertAction(title: "사진앨범", style: .default) { (action) in
+            self.openLibrary()
+        }
+        
+        let camera = UIAlertAction(title: "카메라", style: .default) { (action) in
+            self.openCamera()
+        }
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(library)
+        alert.addAction(camera)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: - Helpers
+    
+    func openLibrary() {
+        imagePickerViewController.sourceType = .photoLibrary
+        present(imagePickerViewController, animated: false, completion: nil)
+    }
+    
+    func openCamera() {
+        imagePickerViewController.sourceType = .camera
+        present(imagePickerViewController, animated: false, completion: nil)
+    }
+}
 
+
+// MARK: - Extensions
+
+extension AddPalateViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerValues[row]
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerValues.count
+    }
+    
+}
+
+extension AddPalateViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image =
+            info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            // 이미지 위치시켜주기
+            productImage.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
